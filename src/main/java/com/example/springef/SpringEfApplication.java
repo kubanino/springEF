bbe63e1b0156520874738fb600ab801a1c823284
Jakub Dubański <jakub.dubanski@gmail.com>
2019-01-01 17:53:35 +0100

    [SpringEF] Initial commit.

---
+++ src/main/java/com/example/springef/SpringEfApplication.java
@@ -0,0 +1,15 @@
+package com.example.springef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEfApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringEfApplication.class, args);
+    }
+
+}
